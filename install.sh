51161c44772fba34a40f4861dfc60e243be66a57
John Crawley <144404770+jmcprivate@users.noreply.github.com>
2025-03-15 20:41:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,19 +53,15 @@ sudo apt clean
 
 # AWS CLI / kubectl 
 # Install AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 rm -rf awscliv2.zip aws
 
 sudo apt install -y ca-certificates curl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
-
-
-
-
 
 # Print the versions installed
 python3 --version
